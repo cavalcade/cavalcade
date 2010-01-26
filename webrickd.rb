257e406b3b1287147e911cf110e3375bb8130130
Sam Solomon <sssbox+cavalcade@gmail.com>
2010-01-25 23:13:22 -0600

    Fixed webrick implementation so that it will work on windows in addition to linux.

--- webrickd.rb
+++ webrickd.rb
@@ -3,10 +3,18 @@
 require 'webrick'
 include WEBrick
 
+if __FILE__.include? "/"
+  DS = "/"
+else
+  DS = "\\"
+end
+
+doc_root = File.dirname(__FILE__)+DS+"static-html"
+
 # Create an HTTP server
 s = HTTPServer.new(
   :Port            => 8000,
-  :DocumentRoot    => "./static-html"
+  :DocumentRoot    => doc_root 
 )
 
 # When the server gets a control-C, kill it
